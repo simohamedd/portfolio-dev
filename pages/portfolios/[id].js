3d95e92d8f1c8a005da721fefe236bb5008e8905
simohamedd <bouazzaouimohamed100@gmail.com>
2025-01-15 08:52:40 +0000

    day8

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 import BaseLayout from "@/components/layouts/BaseLayout";
 import { useRouter } from "next/router";
 
@@ -14,3 +15,38 @@ const PortfolioDetail = () => {
 }
 
 export default PortfolioDetail;
+=======
+
+import BaseLayout from "@/components/layouts/BaseLayout";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const Portfolio = ({post}) => {
+    const router = useRouter();
+    
+    return (
+        <BaseLayout>
+            <h1>I am portfolio page</h1>
+            <h1>{post.title}</h1>
+            <p>BODY: {post.body}</p>
+            <p>ID: {post.id}</p>
+        </BaseLayout>
+    );
+};
+
+
+Portfolio.getInitialProps = async ({query}) => {
+       let post = {}; 
+
+
+       try {
+            const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`);
+            post = res.data       
+        }catch(e){
+           console.error(e);
+       }
+       return { post };
+}
+
+export default Portfolio;
+>>>>>>> temp-branch
